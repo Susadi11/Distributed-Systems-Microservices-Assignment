5b0f70fdd7ce4f3dc994582ce74fe4c50f288d0c
Susadi11 <susadisandanima@gmail.com>
2025-03-25 00:29:27 +0530

    install tailwind

---
+++ frontend-delivery/src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-400 to-purple-800">
+            <h1 className="text-white text-4xl font-bold">🚀 Hello, React! 🚀</h1>
+        </div>
+    );
+}
+
+export default Home;
